29f93e11714564a8372caed11dfdb1c480a42685
Mikołaj Stępczak <stepczak.mikolaj@gmail.com>
2017-05-14 23:44:50 +0200

    poprawki

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
-var app = angular.module('cardsApp', ['ui.router', 'ngAnimate']);
+var app = angular.module('cardsApp', ['ui.router', 'ngAnimate', 'ui-notification']);
 
-app.config(function($stateProvider, $locationProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $locationProvider, $urlRouterProvider, NotificationProvider) {
+    
+    NotificationProvider.setOptions({
+            delay: 3000,
+            startTop: 20,
+            startRight: 10,
+            verticalSpacing: 20,
+            horizontalSpacing: 20,
+            positionX: 'right',
+            positionY: 'bottom'
+    });
+    
     $locationProvider.hashPrefix('')
     $urlRouterProvider.otherwise('/');
   $stateProvider.state({
@@ -12,10 +23,15 @@ app.config(function($stateProvider, $locationProvider, $urlRouterProvider) {
     url: '/list',
     templateUrl: 'list.htm'
   }).state({
-    name: 'config',
-    url: '/config',
-    templateUrl: 'config.htm'
+    name: 'test',
+    url: '/test',
+    templateUrl: 'test.htm'
   });
+//  .state({
+//    name: 'config',
+//    url: '/config',
+//    templateUrl: 'config.htm'
+//  });
 });
 
 app.service('storage', function() {
@@ -29,7 +45,7 @@ app.service('storage', function() {
     };
     
     this.getCards = function() {
-        var result = "", cards = []; 
+        var result = "", cards = [], newItem, firstItem = true; 
         result = localStorage.getItem('fastCards');
         
         if(result == "" || result == null) {
@@ -38,12 +54,43 @@ app.service('storage', function() {
             
         result = result.split('|');
         result.forEach(function(item){
-            cards.push(JSON.parse(item));
+            newItem = JSON.parse(item);
+            if(firstItem) {
+                newItem.active = true;
+                firstItem = false;
+            } else {
+                newItem.active = false;
+            }
+            newItem.hover = false;
+            newItem.success = undefined;
+            cards.push(newItem);
         });
         
         return cards;
     };
 }); 
+
+app.service('helpers', function() {
+   this.shuffle = function(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    } 
+});
+
 
 app.directive("fileread", [function () {
     return {
@@ -64,22 +111,36 @@ app.directive("fileread", [function () {
     }
 }]);
 
-app.controller('cardsCtrl', function($scope, $timeout, storage) {
+app.controller('cardsCtrl', function($scope, $timeout, $interval, storage, helpers, Notification) {
+    
     var vm = this;
     vm.loader = true;
     vm.image = null;
+    vm.imageText = null;
     vm.question = "";
     vm.answer = "";
-    vm.notiSuccess = false;
-    vm.notiWarning = false;
+    vm.history = [];
+    vm.clock = {
+        start: 0,
+        current: 0,
+        interval: 0
+    };
+    vm.tests = [];
     vm.cards = storage.getCards();
+    vm.cardsTest = vm.cards;
+    vm.currentCard = 0;
 
-    vm.cards.forEach(function(item, index){
-        item.hover = false;
-        if(item.active == true) {
-            vm.currentCard = index;        
-        } 
-    });
+    vm.saveHistory = function() {
+        vm.history.push(JSON.stringify(vm.cards));
+    };
+    vm.saveHistory();
+    
+    vm.backHistory = function(){
+        if(vm.history.length > 0) {
+            vm.cards = JSON.parse(vm.history[vm.history.length-2]);
+            vm.history.splice(-1);
+        }
+    };  
     
     vm.toggleHover = function(index) {
         if(vm.cards[index].hover) {
@@ -87,15 +148,7 @@ app.controller('cardsCtrl', function($scope, $timeout, storage) {
         } else {
             vm.cards[index].hover = true;
         }
-    }
-    
-    vm.setPage = function(page) {
-        if(page =='home') {
-            vm.config = false;
-        } else {
-            vm.config = true;
-        }
-    }
+    };
     
     vm.removeCard = function(index) {
         if(vm.cards[index].active) {
@@ -105,12 +158,15 @@ app.controller('cardsCtrl', function($scope, $timeout, storage) {
         if(index <= vm.currentCard) vm.currentCard--;
         vm.cards.splice(index, 1); 
         
+        vm.saveHistory();
     };
+    
     
     vm.addCard = function() {
         if(vm.question != "" && vm.answer != "") {
             vm.cards.push({
                 image: vm.image,
+                imageText: vm.imateText,
                 question: vm.question,
                 answer: vm.answer
             });
@@ -119,15 +175,12 @@ app.controller('cardsCtrl', function($scope, $timeout, storage) {
                 vm.currentCard = 0
             }
             vm.image = null;
+            vm.imageText = null;
             vm.question = "";
             vm.answer = "";
+            vm.saveHistory();
         } else {
-            if(!vm.notiWarning) {
-                vm.notiWarning = true;
-                $timeout(function(){
-                    vm.notiWarning = false;
-                }, 3000);
-            }
+            Notification.warning('Wprowadź pytanie i odpowiedź!');
         }
     };
     
@@ -140,15 +193,61 @@ app.controller('cardsCtrl', function($scope, $timeout, storage) {
                item.hover = false; 
             });
         }
+    };
+    
+    vm.testCard = function(success) {
+        if(success == true) {
+            vm.cards[vm.currentCard].success = true;
+        } else if(success == false){
+            vm.cards[vm.currentCard].success = false;
+        }
+        vm.currentCard += 1;
+        if(vm.currentCard == vm.cards.length) {
+            vm.stopTest();
+        } else {
+            vm.cards[vm.currentCard-1].active=false;
+            vm.cards[vm.currentCard-1].hover=false;
+            vm.cards[vm.currentCard].active=true;
+        }
     }
     
     vm.syncCards = function() {
         if(storage.saveCards(vm.cards) && !vm.notiSuccess) {
-            vm.notiSuccess = true;
-            $timeout(function(){
-                vm.notiSuccess = false;
-            }, 3000);
+            Notification.success("Zapisano aktualny stan fiszek!");
         }
-    }
+    };
     
+    
+    vm.startTest = function(){
+        vm.cards[vm.currentCard].active=false;
+        vm.cards = helpers.shuffle(vm.cards);
+        vm.changeCard(0);
+        vm.clock.start = Date.now();
+        vm.clock.current = Date.now();
+        vm.clock.interval = $interval(function(){
+            vm.clock.current = Date.now();
+        }, 1000);
+    };
+    
+    vm.stopTest = function(){
+        var count = 0, countSuccess = 0, result = [];
+        $interval.cancel(vm.clock.interval);
+        
+        vm.cards.forEach(function(item){
+            if(item.success == true) {
+                countSuccess++;
+            }
+            count++;
+            result.push(item.success);
+            item.success = undefined;
+        });
+        vm.tests.push(result);
+        vm.currentCard = 0;
+        vm.cards[vm.cards.length-1].active = false;
+        vm.cards[0].active = true;
+        
+        Notification.primary("Twój wynik to: " + countSuccess+"/"+ count +"<br/>"+"Twój czas to: "+new Date(vm.clock.current-vm.clock.start-3600000).toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1"));
+        vm.clock.start = 0;
+        vm.clock.current = 0;
+    };
 });
